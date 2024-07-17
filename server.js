6bacdf8d41b6c7bcf3063730792e04660c649efe
Chepolocote <158088835+Chepolocote@users.noreply.github.com>
2024-07-16 22:03:34 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysqlConexion = require("./conexion");
+const usuarios = require("./routes/usuarios");
+const datos = require("./routes/datos");
+const pool = require("./conexion");
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+// Rutas de la API
+app.use(bodyParser.json());
+app.use("/usuarios", usuarios);
+app.use("/datos", datos);
+app.listen(3000);
+
+// Puerto en el que escucha el servidor
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

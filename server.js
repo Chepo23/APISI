7bb0a7794fec0524c82ad82d0a06edb6c001ca7b
Chepo23 <sergiojesus10bf@gmail.com>
2024-07-18 22:12:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,28 @@ const bodyParser = require("body-parser");
 const mysqlConexion = require("./conexion");
 const usuarios = require("./routes/usuarios");
 const datos = require("./routes/datos");
-const pool = require("./conexion");
-
 
 const app = express();
 app.use(bodyParser.json());
 
+// Configuración de CORS
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Manejo de solicitudes OPTIONS
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
     next();
 });
 
 // Rutas de la API
-app.use(bodyParser.json());
 app.use("/usuarios", usuarios);
 app.use("/datos", datos);
-app.listen(3000);
 
 // Puerto en el que escucha el servidor
 const PORT = process.env.PORT || 3001;
